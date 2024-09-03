851ab36abe0dc3295244e5a37db6740e104a5945
dan.nuta <dan.nuta@ebs-integrator.com>
2024-09-03 13:12:32 +0300

    chore: delete node_modules

--- dist/models/posts.js
+++ dist/models/posts.js
@@ -1,2 +1,16 @@
-export {};
-//# sourceMappingURL=posts.js.map
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Posts = void 0;
+const mongoose_1 = require("mongoose");
+const PostSchema = new mongoose_1.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    date: { type: String },
+    author: {
+        name: { type: String },
+        prenume: { type: String },
+        id: { type: String },
+    },
+    img: { type: String, required: true },
+});
+exports.Posts = (0, mongoose_1.model)("Posts", PostSchema);
